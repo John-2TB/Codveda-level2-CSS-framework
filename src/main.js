088c054aba86486f28ea9081a0b10ec3e04b2fb1
John-2TB <johnkoto2tb@gmail.com>
2026-08-03 23:43:42 +0100

    Hero section implemented

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './style.css';
 import { gsap } from 'gsap';
+
+
+
+// *****************************************
+// ************* DOM ELEMENTS **************
+// *****************************************
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navSection = document.querySelector('.nav-section');
+
+
+
+
+
+
+// *****************************************
+// ************* EVENT LISTENERS ***********
+// *****************************************
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  navSection.classList.toggle('active')
+});
