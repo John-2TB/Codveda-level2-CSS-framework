c559c2439b2e5a98dbfacf2dbdea0ed23bf2e6b7
John-2TB <johnkoto2tb@gmail.com>
2026-08-11 02:55:47 +0100

    Implemented footer sectio

---
+++ src/constants/product.js
@@ -0,0 +1,84 @@
+const products = [
+  {
+    id: 1,
+    name: 'Pepsi',
+    subtext: 'Moment worth savouring',
+    src: '/images/product_image_pepsi.avif',
+  },
+  {
+    id: 2,
+    name: 'Pepsi Zero Sugar',
+    subtext: 'ZERO SUGAR, ALL THE PEPSI TASTE',
+    src: '/images/pepsi_zero.avif',
+  },
+  {
+    id: 3,
+    name: 'PEPSI WILD CHERRY',
+    subtext: 'A WILD BURST OF BERRY FLAVOR',
+    src: '/images/pepsi_wild_cherry.avif',
+  },
+  {
+    id: 4,
+    name: 'PEPSI ZERO SUGAR WILD CHERRY',
+    subtext: 'BOLD CHERRY FLAVOR, ZERO SUGAR',
+    src: '/images/pepsi_wild_cherry_zero.avif',
+  },
+  {
+    id: 5,
+    name: 'DIET PEPSI WILD CHERRY',
+    subtext: 'ZERO CALORIES WITH A WILD CHERRY TWIST',
+    src: '/images/pepsi_wild_cherry_diet.avif',
+  },
+  {
+    id: 6,
+    name: 'PEPSI WILD CHERRY & CREAM',
+    subtext: 'SMOOTH CREAMINESS WITH A WILD CHERRY KICK',
+    src: '/images/pepsi_wild_cherry_cream.avif',
+  },
+  {
+    id: 7,
+    name: 'PEPSI ZERO SUGAR WILD CHERRY & CREAM',
+    subtext: 'RICH & CREAMY FLAVOR, ZERO SUGAR',
+    src: '/images/pepsi_wild_cherry_cream_zero.avif',
+  },
+  {
+    id: 8,
+    name: 'PEPSI SODA',
+    subtext: 'VINTAGE REFRESHMENT MADE WITH REAL SUGAR',
+    src: '/images/pepsi_soda.avif',
+  },
+  {
+    id: 9,
+    name: 'PEPSI PREBIOTIC COLA',
+    subtext: 'REFRESHMENT REIMAGINED FOR YOUR WELLNESS',
+    src: '/images/pepsi_probiotic_cola.avif',
+  },
+  {
+    id: 10,
+    name: 'PEPSI PREBIOTIC COLA CHERRY VANILLA',
+    subtext: 'BOLD FLAVOR MEETS GUT WELLNESS',
+    src: '/images/pepsi_probiotic_cherry_vanilla.avif',
+  },
+  {
+    id: 11,
+    name: 'Pepsi Diet',
+    subtext: 'LIGHT, CRISP, AND UNAPOLOGETICALLY REFRESHING',
+    src: '/images/pepsi_diet.avif',
+  },
+  {
+    id: 12,
+    name: 'DIET PEPSI CAFFEINE FREE',
+    subtext: 'ZERO CALORIES, ZERO CAFFEINE, FULL FLAVOR',
+    src: '/images/pepsi_coffeine_free_diet.avif',
+  },
+  {
+    id: 13,
+    name: 'PEPSI CAFFEINE FREE',
+    subtext: 'GREAT PEPSI TASTE, ALL DAY & NIGHT',
+    src: '/images/pepsi_caffeine_free.avif',
+  },
+
+];
+
+
+export {products}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './style.css';
+import {products} from './constants/product.js';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -13,7 +14,79 @@ gsap.registerPlugin(ScrollTrigger);
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navSection = document.querySelector('.nav-section');
-const mobileVideo = document.querySelector('#mobile-video');
+const videoSection = document.querySelector('#videoSection');
+const productDisplay = document.querySelector('#productDisplay');
+const leftButton = document.querySelector('#left-button');
+const rightButton = document.querySelector('#right-button');
+
+let currentIndex = 0;
+
+// *****************************************
+// ************* FUNCTIONS ******************
+// *****************************************
+function renderProducts() {
+  if (productDisplay) {
+  productDisplay.innerHTML = products.map((card) =>
+  `
+    <div class="flex items-center justify-center flex-col min-w-full shrink-0 snap-start">
+      <img src="${card.src}" alt="${card.name}" class="w-60 h-60 object-contain">
+      <div class="flex flex-col items-center justify-center w-60 h-auto">
+        <p class="mt-4 text-clamp(1rem, 2rem, 3rem) flex justify-center items-center">${card.name}</p>
+        <p class="font-inter flex justify-center items-center text-clamp(1rem, 2rem, 3rem) mb-4">${card.subtext}</p>
+        <button class="bg-pepsi-blue px-6 py-3 rounded-4xl hover:bg-pepsi-hover hover:text-pepsi-black transition-all duration-200 hover:cursor-pointer ease-in-out">Find Out More</button>
+      </div>
+    </div>
+  `
+  ).join('');
+
+  } else {
+    return;
+  };
+
+
+  const productCards = [...productDisplay.querySelectorAll('div')];
+  if (!productCards.length) return;
+
+  const cardWidth = productCards[0].offsetWidth;
+  productDisplay.scrollTo({
+    left: currentIndex * cardWidth,
+    behavior: 'smooth'
+  });
+
+};
+
+function updateProductPosition() {
+  const cards = [...document.querySelectorAll('#productDisplay > div')];
+  if (!cards.length) return;
+
+  const cardWidth = cards[0].offsetWidth;
+
+  productDisplay.scrollTo({
+    left: currentIndex * cardWidth,
+    behavior: 'smooth'
+  });
+};
+
+
+function showNextProduct() {
+  const cards = [...document.querySelectorAll('#productDisplay > div')];
+  if (!cards.length) return;
+
+  currentIndex = (currentIndex + 1) % cards.length;
+  updateProductPosition();
+};
+
+function showPreviousProduct() {
+  const cards = [...document.querySelectorAll('#productDisplay > div')];
+  if (!cards.length) return;
+
+  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+  updateProductPosition();
+}
+
+
+leftButton?.addEventListener('click', showPreviousProduct);
+rightButton?.addEventListener('click', showNextProduct);
 
 
 // *****************************************
@@ -49,15 +122,16 @@ const heroTimeline = gsap.timeline({
 });
 
 ScrollTrigger.create({
-  trigger: '#videoSection',
+  trigger: videoSection,
   start: 'top top',
   end: 'center center',
-  onEnter: () => mobileVideo.play(),
-  onLeave: () => mobileVideo.pause(),
-  onEnterBack: () => mobileVideo.play(),
-  onLeaveBack: () => mobileVideo.pause(),
+  onEnter: () => videoSection.play(),
+  onLeave: () =>  videoSection.play(),
+  onEnterBack: () =>  videoSection.play(),
+  onLeaveBack: () =>  videoSection.play(),
 });
 
 
 
 
+renderProducts();
